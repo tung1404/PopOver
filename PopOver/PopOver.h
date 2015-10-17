e6f76583ce44e34f7e3df7f9d77d6f239c1d0ee6
Bertrand Marlier <bertrand.marlier@me.com>
2015-10-17 09:19:56 +0200

    add new PopOver files

---
+++ PopOver/PopOver.h
@@ -0,0 +1,19 @@
+//
+//  PopOver.h
+//  PopOver
+//
+//  Created by Bertrand Marlier on 17/10/2015.
+//  Copyright © 2015 Bertrand Marlier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopOver.
+FOUNDATION_EXPORT double PopOverVersionNumber;
+
+//! Project version string for PopOver.
+FOUNDATION_EXPORT const unsigned char PopOverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopOver/PublicHeader.h>
+
+
